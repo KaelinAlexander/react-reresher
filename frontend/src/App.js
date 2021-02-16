1cb4ce89e0bcfe1499c44f105a15f5c21274dfe3
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-16 10:20:51 -0700

    refactors form submission to use state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
 
-  const courseGoals = [
-                      {id: 'cg1', text: 'Finish the Course'},
-                      {id: 'cg2', text: 'Learn about MERN'},
-                      {id: 'cg3', text: 'Help others Learn'}
-                    ]
+  const [courseGoals, setCourseGoals] = useState([
+  {id: 'cg1', text: 'Finish the Course'},
+  {id: 'cg2', text: 'Learn about MERN'},
+  {id: 'cg3', text: 'Help others Learn'}
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    setCourseGoals(courseGoals.concat(newGoal));
+  }
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <NewGoal />
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals} />
     </div>
   )
