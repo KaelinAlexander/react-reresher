0bbf4fdc04560001f4e1ed1d9fa931e00a7ccbfd
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-16 08:33:49 -0700

    refactors App.js into multiple components; splits styling across multiple files as best practice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import './App.css';
+import GoalList from './components/GoalList';
 
-function App() {
-
-    return (
-      <h1>Hello, from React!</h1>
-    )
-
+const App = () => {
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  )
 }
+
+// function App() {
+
+//     return (
+//       <h1>Hello, from React!</h1>
+//     )
+
+// }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello, from React!</h1>
+//   }
+// }
 
 export default App;
---
+++ frontend/src/components/GoalList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './GoalList.css'
+
+const GoalList = () => {
+
+    return (
+        <ul className="goal-list">
+            <li>Finish the Course</li>
+            <li>Learn about MERN</li>
+            <li>Help other students in QandA</li>
+        </ul>
+    );
+
+}
+
+export default GoalList;
