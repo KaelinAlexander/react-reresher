8fb70e0abfe8d7e9fad89bf258a142e8919a51c0
KaelinAlexander <kaelin.alexander@gmail.com>
2021-02-16 10:04:43 -0700

    adds initial dummy goal form handler and NewGoal form

--- frontend/src/components/GoalList/GoalList.js
+++ frontend/src/components/GoalList/GoalList.js
@@ -7,7 +7,7 @@ const GoalList = (props) => {
         <ul className="goal-list"> 
             {props.goals.map((goal) => {
                     return <li key={goal.id}>{goal.text}</li>
-                })};
+                })}
         </ul>
     );
 
